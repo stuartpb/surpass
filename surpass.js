4f0fa65620eba9925d9190fc280d1ca6a18e3591
Stuart P. Bentley <stuart@testtrack4.com>
2014-03-12 02:26:13 -0400

    Initial script with bit twiddling functions

---
+++ surpass.js
@@ -0,0 +1,18 @@
+(function(root){
+  var surpass = {};
+
+  function twiddlify(value, andmask, ormask, factor) {
+    return (value * (factor || 1)) & (andmask || 0xffffffff) | (ormask || 0);
+  }
+
+  function colorizer(andmask, ormask, factor) {
+    andmask = andmask | 0xffffff;
+    return function(value) {
+      var color = twiddlify(value, andmask, ormask, factor).toString(16);
+      return '#000000'.slice(7-color.length) + color;
+    };
+  }
+  surpass.twiddlify = twiddlify;
+  surpass.colorizer = colorizer;
+  root.surpass = surpass;
+})(typeof module != 'undefined' ? module.exports : window);

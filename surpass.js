f525f202b647e64d08a698e60a36e103a3303fcf
Stuart P. Bentley <stuart@testtrack4.com>
2014-05-20 23:25:06 -0700

    Add bar colorizer

--- surpass.js
+++ surpass.js
@@ -5,14 +5,24 @@
     return (value * (factor || 1)) & (andmask || 0xffffffff) | (ormask || 0);
   }
 
-  function colorizer(andmask, ormask, factor) {
+  function colorTwiddler(andmask, ormask, factor) {
     andmask = andmask & 0xffffff;
     return function(value) {
       var color = twiddlify(value, andmask, ormask, factor).toString(16);
       return '#000000'.slice(0, 7-color.length) + color;
     };
   }
+
+  function barColorizer(bars, toColor) {
+    return function(hash) {
+      for (var i=0; i < bars.length; i++) {
+        bars[i].style.backgroundColor = toColor(hash.slice(i*6, (i+1)*6));
+      }
+    };
+  }
+
   surpass.twiddlify = twiddlify;
-  surpass.colorizer = colorizer;
+  surpass.colorTwiddler = colorTwiddler;
+  surpass.barColorizer = barColorizer;
   root.surpass = surpass;
 })(typeof module != 'undefined' ? module.exports : window);

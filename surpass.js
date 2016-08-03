4a028ab34b9c66154952f76e1423c8cf73f67424
Stuart P. Bentley <stuart@testtrack4.com>
2016-08-03 04:33:42 -0700

    Shadow root experiment thing

--- surpass.js
+++ surpass.js
@@ -7,22 +7,49 @@
 
   function colorTwiddler(andmask, ormask, factor) {
     andmask = andmask & 0xffffff;
-    return function(value) {
+    return function twiddler(value) {
       var color = twiddlify(value, andmask, ormask, factor).toString(16);
       return '#000000'.slice(0, 7-color.length) + color;
     };
   }
 
-  function barColorizer(bars, toColor) {
-    return function(hash) {
-      for (var i=0; i < bars.length; i++) {
-        bars[i].style.backgroundColor = toColor(hash.slice(i*6, (i+1)*6));
+  function twiddlingColorExtractor(twiddler) {
+    return function (hash, i) {
+      return twiddler(hash.slice(i*6, (i+1)*6));
+    };
+  }
+
+  function barColorizer(bars, extractor) {
+    return function (hash) {
+      for (var i = 0; i < bars.length; i++) {
+        bars[i].style.backgroundColor = extractor(hash, i);
       }
     };
   }
 
+  function makeBars(n) {
+    var bars = [];
+    var bar = document.createElement('div');
+    bar.style.height = '100%';
+    bar.style.width = 100 / n + '%';
+    bars[0] = bar;
+    for (var i = 1; i < n; i++) {
+      bars[i] = bar.cloneNode(false);
+    }
+    return bars;
+  }
+
+  function surpassify(input){
+    var sroot = input.createShadowRoot();
+    var ie = document.createElement('shadow');
+    sroot.appendChild(ie);
+    console.log(sroot);
+  }
+
   surpass.twiddlify = twiddlify;
   surpass.colorTwiddler = colorTwiddler;
+  surpass.twiddlingColorExtractor = twiddlingColorExtractor;
   surpass.barColorizer = barColorizer;
+  surpass.surpassify = surpassify;
   root.surpass = surpass;
 })(typeof module != 'undefined' ? module.exports : window);

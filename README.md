1f0a6930b0a9f503152aa686072e7a60d0ec5840
Stuart P. Bentley <stuart@testtrack4.com>
2016-08-05 22:50:56 -0700

    Initial Gross-Simplification-featuring implementation

--- README.md
+++ README.md
@@ -1,4 +1,38 @@
-surpass
-=======
+# surpass
 
-Taking password input beyond the ordinary
+Surpass is an input component for sensitive data, like passwords, that incorporates several innovations in usability:
+
+- Enables toggling of input between unmasked (showing input verbatim), masked (hiding individual characters, ie. traditional password entry), and hidden (all feedback is completely swallowed)
+- Masked input includes an abstract signifier of the masked content, for validation purposes (see the section on "Gross Simplification" below)
+- Validating input by re-typing it is both optional and always available
+
+## Allows Increased / Decreased Obfuscation
+
+Traditional password obfuscation is [frequently][nielsen]
+
+Adding absolute-privacy allows users paranoid about the disclosure of Gross Simplification Ideally, this would also enable functionality in user agents to completely hide feedback *outside* of the password input
+
+[nielsen]: https://www.nngroup.com/articles/stop-password-masking/
+
+Additionally, this configrable sensitivity should be available for *any* kind of input, not just passwords. Some users may be uncomfortable entering their Social Security Number, PIN, or real name in public,
+
+## Gross Simplification
+
+Communicates the input state, a la Lotus's Hieroglyphics or Chroma-Hash, without exposing significant details of the password.
+
+- Doesn't show until the user has gone at least 1/3 second without typing
+- Doesn't show for passwords shorter than 6 characters
+
+### Calculation
+
+This technique maps the entered string to one of 144 different possible combinations of shape, color, and position, by taking the FNV-1a 32-bit hash of the string's UTF-8 representation, then mapping the first multiple of 6 to shapes, the next multiple of 6 to colors, and the remaining multiple of 4 to which position the first shape will be in: the top, right, left, or bottom. (This leaves an unavoidable < 1/10000 bias toward some shapes and colors, but keeps the four positions evenly represented.)
+
+The FNV-1a hash function was selected as the simplest available technique to derive a value from a string (compared to a relatively heavy function like MD5), while still maintaining the properties necessary for this use case ([even distribution / diffusion][1] and non-reversibility).
+
+[1]: http://programmers.stackexchange.com/questions/49550/which-hashing-algorithm-is-best-for-uniqueness-and-speed
+
+## Double-entry
+
+Re-entering something the user can read isn't as sensible, but you may want to recommend they take that extra step: however, there may be other fields where this kind of validation [may make more sense for the user][xkcd 970].
+
+[xkcd 970]: https://xkcd.com/970/
--- surpass.js
+++ surpass.js
@@ -1,55 +1,157 @@
-(function(root){
-  var surpass = {};
-
-  function twiddlify(value, andmask, ormask, factor) {
-    return (value * (factor || 1)) & (andmask || 0xffffffff) | (ormask || 0);
+(function (root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    define([], factory);
+  } else if (typeof module === 'object' && module.exports) {
+    module.exports = factory();
+  } else {
+    root.surpass = factory();
   }
+}(this, function () {
 
-  function colorTwiddler(andmask, ormask, factor) {
-    andmask = andmask & 0xffffff;
-    return function twiddler(value) {
-      var color = twiddlify(value, andmask, ormask, factor).toString(16);
-      return '#000000'.slice(0, 7-color.length) + color;
+  // prime and offset for 32-bit FNV-1
+  // http://www.isthe.com/chongo/tech/comp/fnv/index.html
+  var fnv1Prime = 16777619;
+  var fnv1OffsetBasis = 2166136261;
+  function fnv1a(bytes) {
+    return bytes.reduce(function fnv1aCore(hash, byte) {
+      return (hash ^ byte) * fnv1Prime;
+    }, fnv1OffsetBasis) >>> 0;
+  }
+  function utf8Bytes(str) {
+    return unescape(encodeURIComponent(str)).split('').map(function (s) {
+      return s.charCodeAt(0);
+    });
+  }
+  function grossSimplify(str) {
+    return fnv1a(utf8Bytes(str)) % 144;
+  }
+  var colorSets = ['rby', 'ryb', 'bry', 'byr', 'yrb', 'ybr'];
+  var shapeSets = ['cts', 'cst', 'tcs', 'tsc', 'sct', 'stc'];
+  var arrangements = ['t', 'r', 'b', 'l']; // clockwise from top aka CSS order
+  function colorsShapesArrangement(grossIndex) {
+    return {
+      arrangement: arrangements[grossIndex % 4],
+      colors: colorSets[(grossIndex / 4 | 0) % 6],
+      shapes: shapeSets[grossIndex / 24 | 0]
     };
   }
 
-  function twiddlingColorExtractor(twiddler) {
-    return function (hash, i) {
-      return twiddler(hash.slice(i*6, (i+1)*6));
-    };
+  // positions for shapes to move to, from left to right,
+  // by quarters of the available dimensions
+  var arrPositions = {
+    t: [[1,3], [2,1], [3,3]],
+    r: [[1,1], [1,3], [3,2]],
+    b: [[1,1], [2,3], [3,1]],
+    l: [[1,2], [3,1], [3,3]]
   }
 
-  function barColorizer(bars, extractor) {
-    return function (hash) {
-      for (var i = 0; i < bars.length; i++) {
-        bars[i].style.backgroundColor = extractor(hash, i);
-      }
-    };
-  }
+  var eqTriHeight = Math.sqrt(3)/2;
+  var triHeightCompensation = (1 - eqTriHeight) / 2;
 
-  function makeBars(n) {
-    var bars = [];
-    var bar = document.createElement('div');
-    bar.style.height = '100%';
-    bar.style.width = 100 / n + '%';
-    bars[0] = bar;
-    for (var i = 1; i < n; i++) {
-      bars[i] = bar.cloneNode(false);
+  var dotTransform = 'scale(0.5)';
+
+  function namespaced(classNames) {
+    if (!Array.isArray(classNames)) {
+      classNames = classNames.split(/\s+/g);
     }
-    return bars;
+    return classNames.map(function(className) {
+      return 'surpass_' + className;
+    }).join(' ');
   }
 
-  function surpassify(input){
-    var sroot = input.createShadowRoot();
-    var ie = document.createElement('shadow');
-    sroot.appendChild(ie);
-    console.log(sroot);
-  }
+  var teSurpassContainer = document.createElement('div');
+  teSurpassContainer.className = namespaced('container');
 
-  surpass.twiddlify = twiddlify;
-  surpass.colorTwiddler = colorTwiddler;
-  surpass.twiddlingColorExtractor = twiddlingColorExtractor;
-  surpass.barColorizer = barColorizer;
-  surpass.surpassify = surpassify;
-  root.surpass = surpass;
-})(typeof module != 'undefined' ? module.exports : window);
+  var teInputContainer = document.createElement('div');
+  teInputContainer.className = namespaced('input-container');
+  // the actual input gets added to the container in construction
+
+  var teSurpassButton = document.createElement('button');
+  var teMaskingButton = teSurpassButton.cloneNode(teSurpassButton);
+  teMaskingButton.className = namespaced('button masking-button');
+  var teUnmaskedButton = teSurpassButton.cloneNode(teSurpassButton);
+  teUnmaskedButton.className = namespaced('button unmasked-button');
+  teUnmaskedButton.textContent = decodeURIComponent('%F0%9F%91%81'); // eye
+
+  var teGrossSimplSpot = document.createElement('div');
+  teGrossSimplSpot.className = namespaced(
+    'gross-simpl-spot gross-simpl-round');
+  var teGrossSimplShape = document.createElement('div');
+  teGrossSimplShape.className = namespaced(
+    'gross-simpl-shape gross-simpl-block gross-simpl-dot');
+  teGrossSimplSpot.appendChild(teGrossSimplShape);
+
+  return function surpass(baseInput, opts) {
+    // TODO: verify that `baseInput` is, in fact, a (password) input
+    opts = opts || {};
+    opts.mode = opts.mode ||
+      baseInput.type == 'password' ? 'masked' : 'clear';
+
+    var container = teSurpassContainer.cloneNode(false);
+    var inputContainer = teInputContainer.cloneNode(false);
+    container.appendChild(inputContainer);
+
+    var maskingButton = teMaskingButton.cloneNode(false);
+
+    var spots = [];
+
+    for (var i = 0; i < 3; i++) {
+      spots[i] = teGrossSimplSpot.cloneNode(true);
+      spots[i].style.transform = 'translate(-50%, -50%) ' + dotTransform;
+      spots[i].className += ' ' + namespaced('gross-simpl-pos'+(i+1));
+      maskingButton.appendChild(spots[i]);
+    }
+
+    maskingButton.addEventListener('click', function(evt) {
+      baseInput.type = 'password';
+    });
+
+    container.appendChild(maskingButton);
+
+    var unmaskedButton = teUnmaskedButton.cloneNode(true);
+
+    unmaskedButton.addEventListener('click', function(evt) {
+      baseInput.type = 'text';
+    });
+
+    container.appendChild(unmaskedButton);
+
+    function updateGrossSimplification(value) {
+      var csa = colorsShapesArrangement(grossSimplify(value));
+      var positions = arrPositions[csa.arrangement];
+
+      var xq = maskingButton.clientWidth / 4;
+      var yq = maskingButton.clientHeight / 4;
+
+      for (var i = 0; i < 3; i++) {
+        var spotEl = spots[i];
+        var shapeEl = spotEl.children[0];
+        var pos = positions[i];
+        var color = csa.colors[i];
+        var shape = csa.shapes[i];
+        var spotW = spotEl.clientWidth;
+        var x = xq * pos[0] - spotW / 2;
+        var y = yq * pos[1] - spotW / 2;
+        if (shape == 't') {
+          y += spotW * triHeightCompensation;
+        }
+        spotEl.style.transform = 'translate(' + x + 'px,' + y + 'px)';
+        spotEl.className = namespaced(['gross-simpl-spot',
+          'gross-simpl-pos0',
+          shape == 'c' ? 'gross-simpl-round' : 'gross-simpl-pointy']);
+        shapeEl.className = namespaced(['gross-simpl-shape',
+          shape == 't' ? 'gross-simpl-tri' : 'gross-simpl-block',
+          'gross-simpl-' + color]);
+      }
+    }
+
+    baseInput.addEventListener('input', function(evt) {
+      // TODO: get clever with animation / timeouts
+      updateGrossSimplification(baseInput.value);
+    });
+
+    baseInput.parentNode.replaceChild(container, baseInput);
+    inputContainer.appendChild(baseInput);
+    return container;
+  };
+}));

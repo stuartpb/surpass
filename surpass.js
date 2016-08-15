7699c1d73211ec454f4365452c2147b06b9785bc
Stuart P. Bentley <stuart@testtrack4.com>
2016-08-15 09:19:44 -0700

    Add double-checking element

--- surpass.js
+++ surpass.js
@@ -59,8 +59,21 @@
     };
   }
 
+  var glyphs = {
+    eye: decodeURIComponent("%F0%9F%91%81%EF%B8%8E"),
+    blackSquare: decodeURIComponent("%E2%97%BC%EF%B8%8F"),
+    check: decodeURIComponent("%E2%9C%94%EF%B8%8F"),
+    rightTri: decodeURIComponent("%E2%96%B6%EF%B8%8F"),
+    downTri: decodeURIComponent("%E2%96%BC"),
+    notEqual: decodeURIComponent("%E2%89%A0"),
+    arrows: decodeURIComponent("%E2%87%85")
+  };
+
   var teSurpassContainer = document.createElement('div');
   teSurpassContainer.className = namespaced('container');
+
+  var teSurpassRowContainer = document.createElement('div');
+  teSurpassRowContainer.className = namespaced('row');
 
   var teInputContainer = document.createElement('div');
   teInputContainer.className = namespaced('input-container');
@@ -69,16 +82,18 @@
   teInputShroud.className = namespaced('input-shroud');
 
   var teSurpassButton = document.createElement('button');
-  var teMaskModeButton = teSurpassButton.cloneNode(teSurpassButton);
-  teMaskModeButton.className = namespaced('button mask-button');
-  var teExposeModeButton = teSurpassButton.cloneNode(teSurpassButton);
-  teExposeModeButton.className = namespaced('button expose-button');
-  teExposeModeButton.textContent =
-    decodeURIComponent('%F0%9F%91%81%EF%B8%8E'); // eye
-  var teShroudModeButton = teSurpassButton.cloneNode(teSurpassButton);
-  teShroudModeButton.className = namespaced('button shroud-button');
-  teShroudModeButton.textContent =
-    decodeURIComponent("%E2%97%BC%EF%B8%8F"); // black square
+  var teMaskModeButton = teSurpassButton.cloneNode(false);
+  teMaskModeButton.className = namespaced('mode-button mask-button');
+  var teExposeModeButton = teSurpassButton.cloneNode(false);
+  teExposeModeButton.className = namespaced('mode-button expose-button');
+  teExposeModeButton.textContent = glyphs.eye;
+  var teShroudModeButton = teSurpassButton.cloneNode(false);
+  teShroudModeButton.className = namespaced('mode-button shroud-button');
+  teShroudModeButton.textContent = glyphs.blackSquare;
+  var teToggleDoubleButton = teSurpassButton.cloneNode(false);
+  var toggleButtonBaseClassName =
+    namespaced('double-toggle-button check-state');
+  teToggleDoubleButton.className = toggleButtonBaseClassName;
 
   var maskSpotClassPrefix = namespaced(
     'gross-simpl-spot gross-simpl-round gross-simpl-x');
@@ -90,11 +105,8 @@
   teGrossSimplShape.className = maskDotShapeClassName;
   teGrossSimplSpot.appendChild(teGrossSimplShape);
 
-  return function surpass(baseInput, opts) {
-    // TODO: verify that `baseInput` is, in fact, a (password) input
-    opts = opts || {};
-
-    var container = teSurpassContainer.cloneNode(false);
+  function surpassifyInput(baseInput, opts, announceModeChange) {
+    var container = teSurpassRowContainer.cloneNode(false);
     var inputContainer = teInputContainer.cloneNode(false);
     container.appendChild(inputContainer);
 
@@ -149,7 +161,7 @@
       }
     }
 
-    var typingWait = (opts.typingWait === 0 ? 0 : opts.typingWait || 500);
+    var typingWait = opts.typingWait;
     var grossSimplificationTimeout = null;
     function stopGrossSimplificationTimeout() {
       if (grossSimplificationTimeout) {
@@ -230,22 +242,28 @@
       }
       mode = newMode;
     }
-    setMode(opts.mode ||
-      baseInput.type == 'password' ? 'mask' : 'expose');
+    setMode(opts.mode);
 
     inputShroud.addEventListener('click', function(evt) {
       baseInput.focus();
     });
 
+    // Inserting the original input is deferred until after construction,
+    // so that it may retain its position in the document until the final
+    // constructed container is ready to be inserted in its place.
+    // Once the baseInput is replaced, it can finally be prepended into
+    // this container; until then, we just append the shroud.
+    inputContainer.appendChild(inputShroud);
+
     shroudModeButton.addEventListener('click', function(evt) {
-      setMode(mode == 'shroud' ? 'mask' : 'shroud');
+      announceModeChange(mode == 'shroud' ? 'mask' : 'shroud');
       baseInput.focus();
     });
 
     container.appendChild(shroudModeButton);
 
     maskModeButton.addEventListener('click', function(evt) {
-      setMode('mask');
+      announceModeChange('mask');
     });
 
     container.appendChild(maskModeButton);
@@ -253,7 +271,7 @@
     var exposeModeButton = teExposeModeButton.cloneNode(true);
 
     exposeModeButton.addEventListener('click', function(evt) {
-      setMode(mode == 'expose' ? 'mask' : 'expose');
+      announceModeChange(mode == 'expose' ? 'mask' : 'expose');
     });
 
     container.appendChild(exposeModeButton);
@@ -281,9 +299,120 @@
       inputShroud.textContent = '';
     });
 
+    return {
+      container: container,
+      inputContainer: inputContainer,
+      setMode: setMode
+    };
+  }
+
+  function doubleSurpassify(baseInput, opts) {
+    var baseRow, checkRow;
+    function setModeBothRows(mode) {
+      baseRow.setMode(mode);
+      checkRow.setMode(mode);
+    }
+
+    var checkInput = document.createElement('input');
+
+    // will be inverted again for first update
+    var doubleChecking = !opts.double;
+
+    checkInput.type = baseInput.type;
+    baseRow = surpassifyInput(baseInput, opts, setModeBothRows);
+    checkRow = surpassifyInput(checkInput, opts, setModeBothRows);
+    checkRow.container.classList.add(namespaced('check-row'));
+
+    // TODO: make these configurable etc.
+    var checkMessages = {
+      available:
+        [glyphs.rightTri, glyphs.check, glyphs.check, '?'].join(' '),
+      prompting:
+        [glyphs.downTri, glyphs.arrows, '_'].join(' '),
+      unmatching:
+        [glyphs.downTri, glyphs.arrows, glyphs.notEqual].join(' '),
+      matching:
+        [glyphs.downTri, glyphs.arrows, '='].join(' ')
+    };
+
+    var toggleButton = teToggleDoubleButton.cloneNode(false);
+    function setDoubleCheckState(stateName) {
+      toggleButton.textContent = checkMessages[stateName];
+      toggleButton.className = toggleButtonBaseClassName + ' ' +
+        namespaced('check-' + stateName);
+    }
+    function updateDoubleCheckState() {
+      if (doubleChecking) {
+        var checkValue = checkInput.value;
+        var baseValue = baseInput.value;
+        if (checkValue == '') {
+          setDoubleCheckState('prompting');
+        } else if (baseValue == checkValue) {
+          setDoubleCheckState('matching');
+        } else {
+          setDoubleCheckState('unmatching');
+        }
+      }
+    }
+    baseInput.addEventListener('input', updateDoubleCheckState);
+    checkInput.addEventListener('input', updateDoubleCheckState);
+    function toggleDoubleChecking() {
+      doubleChecking = !doubleChecking;
+      if (doubleChecking) {
+        checkRow.container.hidden = false;
+        // TODO: clear previous checked value?
+        updateDoubleCheckState();
+      } else {
+        checkRow.container.hidden = true;
+        setDoubleCheckState('available');
+      }
+    }
+    toggleButton.addEventListener('click', toggleDoubleChecking);
+    toggleDoubleChecking();
+
+    var container = teSurpassContainer.cloneNode(false);
+    container.appendChild(baseRow.container);
+    container.appendChild(toggleButton);
+    container.appendChild(checkRow.container);
+
     baseInput.parentNode.replaceChild(container, baseInput);
-    inputContainer.appendChild(baseInput);
-    inputContainer.appendChild(inputShroud);
+    baseRow.inputContainer.insertBefore(baseInput,
+      baseRow.inputContainer.firstChild);
+    checkRow.inputContainer.insertBefore(checkInput,
+      checkRow.inputContainer.firstChild);
+
     return container;
+  }
+
+  function singleSurpassify(baseInput, opts) {
+    var baseRow;
+    function propogateModeBack(mode) {
+      return baseRow.setMode(mode);
+    }
+    baseRow = surpassifyInput(baseInput, opts, propogateModeBack);
+    baseInput.parentNode.replaceChild(baseRow.container, baseInput);
+    baseRow.inputContainer.insertBefore(baseInput,
+      baseRow.inputContainer.firstChild);
+    return baseRow.container;
+  }
+
+  return function surpass(baseInput, optObj) {
+    // TODO: verify that `baseInput` is, in fact, a (password) input
+    function intOrDefault(x, def) {
+      x = parseInt(x, 10);
+      return x === 0 ? 0 : x || def;
+    }
+    var opts = {
+      mode: optObj && optObj.mode ||
+        baseInput.type == 'password' ? 'mask' : 'expose',
+      forceSingle: optObj && optObj.forceSingle,
+      double: optObj && optObj.double,
+      typingWait: intOrDefault(optObj && optObj.typingWait, 500)
+    };
+    if (opts.forceSingle) {
+      return singleSurpassify(baseInput, opts);
+    } else {
+      return doubleSurpassify(baseInput, opts);
+    }
   };
 }));
